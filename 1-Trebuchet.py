7c2d35d32df68e6cf5d3aaa184880d42d5c564ab
Stefan Matthias Kranz <stefan.kranz@student.uni-halle.de>
2023-12-19 13:54:56 +0100

    day 1, part 2

--- 1-Trebuchet.py
+++ 1-Trebuchet.py
@@ -1,10 +1,13 @@
 import re
 
-with open("1-Trebuchet.txt", "r") as file:
-    sum = 0
-    for line in file:
-        numbers = re.sub('[a-z]', '', line)[0:-1]
-        value = int(numbers[0] + numbers[-1])
-        sum += value
+def part_one():
+    with open("1-Trebuchet.txt", "r") as file:
+        sum = 0
+        for line in file:
+            numbers = re.sub('[a-z]', '', line)[0:-1]
+            value = int(numbers[0] + numbers[-1])
+            sum += value
+    return sum
 
-print(sum)
+def part_two():
+    pass
