d8b984d5fe2b635e9635a7aba9af97ac8d81736b
Stefan Matthias Kranz <stefan.kranz@student.uni-halle.de>
2023-12-19 13:52:09 +0100

    bugfix

--- 1-Trebuchet.py
+++ 1-Trebuchet.py
@@ -3,8 +3,8 @@ import re
 with open("1-Trebuchet.txt", "r") as file:
     sum = 0
     for line in file:
-        numbers = re.sub('[a-z]+', '', line)[0:-1]
+        numbers = re.sub('[a-z]', '', line)[0:-1]
         value = int(numbers[0] + numbers[-1])
-        print(value) # FIXME one digit not interpreted right
+        sum += value
 
 print(sum)
