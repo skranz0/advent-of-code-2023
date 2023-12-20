76192fdc02afddb98caba2c07e8d02ec9119db77
Stefan Matthias Kranz <stefan.kranz@student.uni-halle.de>
2023-12-20 12:05:16 +0100

    setup day 2

---
+++ 2-CubeConundrum.py
@@ -0,0 +1,15 @@
+bag = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+class game:
+    def __init__(self, id, games: list(str)):
+        self.id = id
+        self.games = games
+
+
+
+with open("2-tiny.txt", "r") as file:
+    pass
