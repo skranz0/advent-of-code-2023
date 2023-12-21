d50498a5efebce33112b1d2b12e610a654bcbffa
Stefan Matthias Kranz <stefan.kranz@student.uni-halle.de>
2023-12-21 12:23:43 +0100

    parse sets

--- 2-CubeConundrum.py
+++ 2-CubeConundrum.py
@@ -1,3 +1,5 @@
+import re
+
 bag = {
     "red": 12,
     "green": 13,
@@ -5,11 +7,18 @@ bag = {
 }
 
 class game:
-    def __init__(self, id, games: list(str)):
+    def __init__(self, id, games):
         self.id = id
         self.games = games
 
 
 
 with open("2-tiny.txt", "r") as file:
-    pass
+    games = []
+    for line in file:
+        line = re.sub("\n| |Game", "", line)
+        id = int(line.split(":")[0])
+        line = re.sub("[0-9]:", "", line)
+        sets = line.split(";")
+        for s in sets:
+            s = s.split(",")
